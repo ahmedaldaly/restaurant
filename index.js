65fe422ad70bc5cb0227bfa283b23bb79c4f3a2a
Ahmed Eldaly <96292450+ahmedaldaly@users.noreply.github.com>
2025-05-23 19:50:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin:'https://restaurant-front-iota.vercel.app/',
   credentials: true,
 }));
 
